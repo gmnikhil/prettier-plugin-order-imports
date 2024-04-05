ac336c1b9e0865518940565ca3694e1328fb5d9d
gmnikhil <gm.nikhilofficial@gmail.com>
2024-04-05 13:55:11 +0530

    handled import namespaces

--- package/sorter.js
+++ package/sorter.js
@@ -10,7 +10,20 @@ const sortImportsByComponent = (ast, options) => {
   imports.sort((a, b) => {
     const a_specifier = a.specifiers.length ? a.specifiers[0].local : { name: "" }
     const b_specifier = b.specifiers.length ? b.specifiers[0].local : { name: "" }
-
+    if (
+      a_specifier.name &&
+      a.specifiers[0].type === "ImportNamespaceSpecifier" &&
+      (!b_specifier.name || b.specifiers[0].type !== "ImportNamespaceSpecifier")
+    ) {
+      return -1
+    }
+    if (
+      b_specifier.name &&
+      b.specifiers[0].type === "ImportNamespaceSpecifier" &&
+      (!a_specifier.name || a.specifiers[0].type !== "ImportNamespaceSpecifier")
+    ) {
+      return 1
+    }
     if (!a_specifier.name && b_specifier.name) return 1
     if (a_specifier.name && !b_specifier.name) return -1
 
